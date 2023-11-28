31513a5affbb4d8b58ca736fd939d5d2c73af767
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-11-28 23:35:56 +0100

    test: Allow to provide path to Helm chart on command line

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,17 @@ import os.path
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption("--chart-path", help="Path of the Helm chart to test")
+
+
 @pytest.fixture()
-def chart_test_deployment():
-    """Absolute path to the Helm chart `test-deployment`."""
-    tests_path = os.path.dirname(os.path.abspath(__file__))
-    return os.path.normpath(os.path.join(tests_path, "../helm/test-deployment"))
+def chart_test_deployment(pytestconfig):
+    """
+    Path to the Helm chart which shall be tested.
+    """
+    chart_path = pytestconfig.option.chart_path
+    if not chart_path:
+        tests_path = os.path.dirname(os.path.abspath(__file__))
+        chart_path = os.path.normpath(os.path.join(tests_path, "../helm/test-deployment"))
+    return chart_path
