1ec75a4626af49341df37a6e70725e3ef44db0d7
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-11-29 23:35:38 +0100

    test: Ensure that the default global registry points to the knut domain

--- tests/best_practices/test_image_configuration.py
+++ tests/best_practices/test_image_configuration.py
@@ -14,3 +14,20 @@ def test_global_registry_is_used_as_default(helm, chart_path):
     expected_registry = "stub-global-registry"
     image = findone(deployment, "spec.template.spec.containers[0].image")
     assert image.startswith(expected_registry + "/")
+
+
+def test_global_registry_is_using_knut_registry_per_default(helm, chart_path):
+    """
+    The UMS Charts point to the internal registry in the knut domain.
+
+    This shall change once the public registry for the publication of UMS stack
+    artifacts is in place. Until then the default configuration of all plain
+    UMS charts shall use the knut registry by default.
+    """
+    values = {}
+    result = helm.helm_template(chart_path, values)
+    deployment = helm.get_resource(result, kind="Deployment")
+
+    expected_registry = "gitregistry.knut.univention.de"
+    image = findone(deployment, "spec.template.spec.containers[0].image")
+    assert image.startswith(expected_registry + "/")
