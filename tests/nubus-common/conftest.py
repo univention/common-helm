9f538eab5aa376a9b3b0926d92ed5b55c7df056b
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-22 15:12:56 +0100

    test: First test to verify the setup for nubus-common

---
+++ tests/nubus-common/conftest.py
@@ -0,0 +1,21 @@
+
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import os.path
+
+import pytest
+
+
+@pytest.fixture()
+def chart_path(pytestconfig):
+    """
+    Path to the Helm chart which shall be tested.
+    """
+    chart_path = pytestconfig.option.chart_path
+    if not chart_path:
+        tests_path = os.path.dirname(os.path.abspath(__file__))
+        chart_path = os.path.normpath(
+            os.path.join(tests_path, "../../helm/test-nubus-common")
+        )
+    return chart_path
---
+++ tests/nubus-common/test_client_secret.py
@@ -0,0 +1,19 @@
+from yaml import safe_load
+
+from utils import findone
+
+
+def test_client_auth_plain_values_generate_secret(helm, chart_path):
+    values = safe_load(
+        """
+        client:
+          connection:
+            url: "local_stub"
+          auth:
+            username: "stub-username"
+            password: "stub-password"
+    """)
+    result = helm.helm_template(chart_path, values)
+    secret = helm.get_resource(result, kind="Secret", name="release-name-test-nubus-common-client")
+
+    assert findone(secret, "stringData.password") == "stub-password"
