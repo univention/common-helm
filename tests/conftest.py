8b631e02dc6892ae64a1d6ff0a080bab0733d870
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-03-29 11:30:58 +0100

    fix: Add missed conftest file in pytest-helm unittests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-pytest_plugins = "pytester"
-
-
 def pytest_addoption(parser):
     parser.addoption("--chart-path", help="Path of the Helm chart to test")
