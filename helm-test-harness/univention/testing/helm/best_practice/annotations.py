ae259f1f36f41cf801d64a964ffc68bb8c29ac7e
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-24 07:57:52 +0200

    feat(helm-test-harness): Add best practice checks for labels and annotations
    Issue: univention/dev/internal/team-nubus#892

---
+++ helm-test-harness/univention/testing/helm/best_practice/annotations.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from yaml import safe_load
+
+
+class Annotations:
+    """
+    Checks the common annotations behavior expected for all chart resources.
+
+    This is based on the suggestion from the best practices chart of openDesk:
+    https://gitlab.opencode.de/bmi/opendesk/components/platform-development/charts/opendesk-best-practises
+    """
+
+    def test_additional_annotations_add_another_annotation(self, chart, subtests):
+        values = safe_load(
+            """
+            additionalAnnotations:
+              local.test/name: "value"
+            """)
+        result = chart.helm_template(values)
+        for resource in result:
+            with subtests.test(kind=resource["kind"], name=resource["metadata"]["name"]):
+                annotations = resource.findone("metadata.annotations")
+                assert annotations["local.test/name"] == "value"
+
+    def test_additional_annotations_value_is_templated(self, chart, subtests):
+        values = safe_load(
+            """
+            global:
+              test: "stub-value"
+            additionalAnnotations:
+              local.test/name: "{{ .Values.global.test }}"
+            """)
+        result = chart.helm_template(values)
+        for resource in result:
+            with subtests.test(kind=resource["kind"], name=resource["metadata"]["name"]):
+                annotations = resource.findone("metadata.annotations")
+                assert annotations["local.test/name"] == "stub-value"
