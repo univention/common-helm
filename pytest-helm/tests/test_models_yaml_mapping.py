8daf536fe0b484027319df2d4b9e675947648803
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-27 20:53:13 +0200

    test(pytest-helm): Move findone / findall related tests into correct module
    Issue: univention/dev/internal/team-nubus#892

--- pytest-helm/tests/test_models_yaml_mapping.py
+++ pytest-helm/tests/test_models_yaml_mapping.py
@@ -24,3 +24,43 @@ def test_findone_raises_key_error_if_key_is_missing():
     value = YamlMapping()
     with pytest.raises(KeyError):
         value.findone("test.path")
+
+
+stub_data = {
+    "a": {
+        "b": "value",
+        "c": None,
+    },
+}
+
+
+@pytest.mark.parametrize("path,expected_value", [
+    ("a.b", "value"),
+    ("a.c", None),
+])
+def test_findone_returns_found_object(path, expected_value):
+    result = YamlMapping.findone(stub_data, path)
+    assert result == expected_value
+
+
+def test_findone_raises_in_case_of_missing_value():
+    with pytest.raises(KeyError):
+        YamlMapping.findone(stub_data, "a.missing-key")
+
+
+@pytest.mark.parametrize(
+    "path,expected_value",
+    [
+        ("a.b", ["value"]),
+        ("a.c", [None]),
+        ("a.*", ["value", None]),
+    ],
+)
+def test_findall_returns_all_found_objects_in_a_list(path, expected_value):
+    result = YamlMapping.findall(stub_data, path)
+    assert result == expected_value
+
+
+def test_findall_returns_empty_list_in_case_of_missing_value():
+    result = YamlMapping.findall(stub_data, "a.missing-key")
+    assert result == []
--- pytest-helm/tests/test_utils.py
+++ pytest-helm/tests/test_utils.py
@@ -1,49 +1,6 @@
-import pytest
-
-from pytest_helm._yaml import YamlMapping
 from pytest_helm.utils import add_jsonpath_prefix
 
 
 def test_add_jsonpath_prefix():
     values = add_jsonpath_prefix("config.logLevel", None)
     assert values == {"config": {"logLevel": None}}
-
-
-stub_data = {
-    "a": {
-        "b": "value",
-        "c": None,
-    },
-}
-
-
-@pytest.mark.parametrize("path,expected_value", [
-    ("a.b", "value"),
-    ("a.c", None),
-])
-def test_findone_returns_found_object(path, expected_value):
-    result = YamlMapping.findone(stub_data, path)
-    assert result == expected_value
-
-
-def test_findone_raises_in_case_of_missing_value():
-    with pytest.raises(KeyError):
-        YamlMapping.findone(stub_data, "a.missing-key")
-
-
-@pytest.mark.parametrize(
-    "path,expected_value",
-    [
-        ("a.b", ["value"]),
-        ("a.c", [None]),
-        ("a.*", ["value", None]),
-    ],
-)
-def test_findall_returns_all_found_objects_in_a_list(path, expected_value):
-    result = YamlMapping.findall(stub_data, path)
-    assert result == expected_value
-
-
-def test_findall_returns_empty_list_in_case_of_missing_value():
-    result = YamlMapping.findall(stub_data, "a.missing-key")
-    assert result == []
