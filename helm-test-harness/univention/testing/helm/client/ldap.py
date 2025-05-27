3fb9dc51722ae6b938e503c85aab013918e43ad6
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-27 20:56:21 +0200

    feat(helm-test-harness): Add checks for ldap client configuration
    Issue: univention/dev/internal/team-nubus#892

---
+++ helm-test-harness/univention/testing/helm/client/ldap.py
@@ -0,0 +1,250 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from contextlib import nullcontext as does_not_raise
+import subprocess
+
+import pytest
+
+from .base import BaseTest
+
+
+class Ldap(BaseTest):
+    """
+    LDAP access configuration related checks.
+    """
+
+    config_map_name = None
+    secret_name = "release-name-test-nubus-common-ldap"
+
+    default_bind_dn = "cn=admin,dc=univention-organization,dc=intranet"
+
+    path_ldap_bind_dn = "data.LDAP_HOST_DN"
+    path_main_container = "spec.template.spec.containers[?@.name=='main']"
+    path_volume_secret_ldap = "spec.template.spec.volumes[?@.name=='secret-ldap']"
+
+    sub_path_ldap_volume_mount = "volumeMounts[?@.name=='secret-ldap']"
+
+    def test_auth_plain_values_generate_secret(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                bindDn: "stub-bind-dn"
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+
+        assert secret.findone("stringData.password") == "stub-password"
+
+    def test_auth_plain_values_provide_bind_dn_via_config_map(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                bindDn: "stub-bind-dn"
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        config_map = result.get_resource(kind="ConfigMap", name=self.config_map_name)
+        assert config_map.findone(self.path_ldap_bind_dn) == "stub-bind-dn"
+
+    def test_auth_plain_values_bind_dn_is_templated(self, chart):
+        values = self.load_and_map(
+            """
+            global:
+              test: "stub-value"
+            ldap:
+              auth:
+                bindDn: "{{ .Values.global.test }}"
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        config_map = result.get_resource(kind="ConfigMap", name=self.config_map_name)
+        assert config_map.findone(self.path_ldap_bind_dn) == "stub-value"
+
+    def test_auth_plain_values_password_is_not_templated(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                bindDn: "stub-bind-dn"
+                password: "{{ value }}"
+            """)
+        result = chart.helm_template(values)
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        assert secret.findone("stringData.password") == "{{ value }}"
+
+    def test_auth_plain_values_password_is_required(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                bindDn: "stub-bind-dn"
+                password: null
+            """)
+        with pytest.raises(subprocess.CalledProcessError) as error:
+            chart.helm_template(values)
+        assert "password has to be supplied" in error.value.stderr
+
+    @pytest.mark.parametrize("value", [
+        "null",
+        '""',
+    ])
+    def test_auth_bind_dn_is_required(self, chart, value):
+        values = self.load_and_map(
+            f"""
+            ldap:
+              auth:
+                bindDn: {value}
+                password: "stub-password"
+            """)
+        with pytest.raises(subprocess.CalledProcessError) as error:
+            chart.helm_template(values)
+        assert '"ldap.auth.bindDn" is required.' in error.value.stderr
+
+    def test_auth_bind_dn_has_default(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        config_map = result.get_resource(kind="ConfigMap", name=self.config_map_name)
+        assert config_map.findone(self.path_ldap_bind_dn) == self.default_bind_dn
+
+    def test_auth_existing_secret_does_not_generate_a_secret(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        with pytest.raises(LookupError):
+            result.get_resource(kind="Secret", name=self.secret_name)
+
+    def test_auth_existing_secret_does_not_require_plain_password(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                password: null
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        with does_not_raise():
+            chart.helm_template(values)
+
+    def test_auth_existing_secret_mounts_password(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        deployment = result.get_resource(kind="StatefulSet")
+        secret_ldap_volume = deployment.findone(self.path_volume_secret_ldap)
+        assert secret_ldap_volume.findone("secret.secretName") == "stub-secret-name"
+
+    def test_auth_existing_secret_mounts_correct_default_key(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        deployment = result.get_resource(kind="StatefulSet")
+        main_container = deployment.findone(self.path_main_container)
+        secret_ldap_volume_mount = main_container.findone(self.sub_path_ldap_volume_mount)
+
+        assert secret_ldap_volume_mount["subPath"] == "password"
+
+    def test_auth_disabling_existing_secret_by_setting_it_to_null(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                bindDn: "stub-bind-dn"
+                password: "stub-password"
+                existingSecret: null
+            """)
+        result = chart.helm_template(values)
+        deployment = result.get_resource(kind="StatefulSet")
+        secret_ldap_volume = deployment.findone(self.path_volume_secret_ldap)
+        main_container = deployment.findone(self.path_main_container)
+        secret_ldap_volume_mount = main_container.findone(self.sub_path_ldap_volume_mount)
+
+        assert secret_ldap_volume_mount["subPath"] == "password"
+        assert secret_ldap_volume.findone("secret.secretName") == self.secret_name
+
+    def test_auth_existing_secret_mounts_correct_custom_key(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+                  keyMapping:
+                    password: "stub_password_key"
+            """)
+        result = chart.helm_template(values)
+        deployment = result.get_resource(kind="StatefulSet")
+        main_container = deployment.findone(self.path_main_container)
+        secret_ldap_volume_mount = main_container.findone(self.sub_path_ldap_volume_mount)
+
+        assert secret_ldap_volume_mount["subPath"] == "stub_password_key"
+
+    def test_auth_existing_secret_has_precedence(self, chart):
+        values = self.load_and_map(
+            """
+            ldap:
+              auth:
+                password: stub-plain-password
+                existingSecret:
+                  name: "stub-secret-name"
+                  keyMapping:
+                    password: "stub_password_key"
+            """)
+        result = chart.helm_template(values)
+        with pytest.raises(LookupError):
+            result.get_resource(kind="Secret", name=self.secret_name)
+
+        deployment = result.get_resource(kind="StatefulSet")
+        secret_ldap_volume = deployment.findone(self.path_volume_secret_ldap)
+        main_container = deployment.findone(self.path_main_container)
+        secret_ldap_volume_mount = main_container.findone(self.sub_path_ldap_volume_mount)
+
+        assert secret_ldap_volume_mount["subPath"] == "stub_password_key"
+        assert secret_ldap_volume.findone("secret.secretName") == "stub-secret-name"
+
+    def test_global_secrets_keep_is_ignored(self, chart):
+        """
+        Keeping Secrets shall not be supported in Client role.
+
+        Random values for a password will never be generated when in Client
+        role. This is why the configuration `global.secrets.keep` shall not
+        have any effect on Secrets in Client role.
+        """
+        values = self.load_and_map(
+            """
+            global:
+              secrets:
+                keep: true
+
+            ldap:
+              auth:
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        annotations = secret.findone("metadata.annotations", default={})
+        helm_resource_policy = annotations.get("helm.sh/resource-policy")
+        assert helm_resource_policy != "keep"
