4792cd97fc2658754fe850bd1c493c9da09255b9
Johannes Lohmer <lohmer@univention.de>
2025-06-03 14:20:00 +0200

    feat(helm-test-harness): Test env variables with a default value in the helm template. With e.g. the coalesce or default functions
    univention/dev/internal/team-nubus#1173

--- helm-test-harness/univention/testing/helm/configmap.py
+++ helm-test-harness/univention/testing/helm/configmap.py
@@ -5,11 +5,11 @@ import subprocess
 
 import pytest
 
-from univention.testing.helm.base import Base, Labels, Namespace
+from univention.testing.helm.base import Annotations, Base, Labels, Namespace
 from pytest_helm.utils import add_jsonpath_prefix
 
 
-class ConfigMap(Labels, Namespace):
+class ConfigMap(Labels, Namespace, Annotations):
     ...
 
 
@@ -46,3 +46,20 @@ class OptionalEnvVariables(Base):
         configmap = self.helm_template_file(helm, chart_path, values, self.template_file)
         with pytest.raises(KeyError):
             configmap['data'][env_var]
+
+
+class DefaultEnvVariables(Base):
+
+    def test_can_be_set(self, helm, chart_path, key, env_var):
+        stub_value = "custom-stub-value"
+        values = add_jsonpath_prefix(key, stub_value)
+
+        configmap = self.helm_template_file(helm, chart_path, values, self.template_file)
+        assert configmap['data'][env_var] == stub_value
+
+    @pytest.mark.parametrize("value", ["", None])
+    def test_can_be_unset(self, helm, chart_path, key, env_var, value):
+        values = add_jsonpath_prefix(key, value)
+
+        configmap = self.helm_template_file(helm, chart_path, values, self.template_file)
+        assert configmap['data'][env_var], "Config map key: {env_var} has no default value."
