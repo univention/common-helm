b62c3615506eea78b9a7c6650065551f2f136356
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-17 23:07:00 +0200

    test(nubus-common): Adjust udm tests to updated class structure in helm-test-harness

--- tests/nubus-common/test_client_secret.py
+++ tests/nubus-common/test_client_secret.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.udm import UdmClient
+from univention.testing.helm.client.udm import Auth, Connection, SecretViaVolume
 
 
-class TestUdmClientConfiguration(UdmClient):
+class TestAuth(SecretViaVolume, Auth):
 
     # TODO: Intentionally kept the prefix "client" to ensure that support for
     # mapping is sufficient. Change back and test in a different way.
@@ -14,8 +14,17 @@ class TestUdmClientConfiguration(UdmClient):
         "global.client": "global.udm",
     }
 
-    path_udm_api_url = "data.CLIENT_API_URL"
-    path_udm_api_username = "data.CLIENT_API_USERNAME"
+    path_username = "data.CLIENT_API_USERNAME"
     path_volume = "spec.template.spec.volumes[?@.name=='secret-client']"
     sub_path_volume_mount = "volumeMounts[?@.name=='secret-client']"
     secret_name = "release-name-test-nubus-common-client"
+
+
+class TestConnection(Connection):
+
+    prefix_mapping = {
+        "client": "udm",
+        "global.client": "global.udm",
+    }
+
+    path_url = "data.CLIENT_API_URL"
