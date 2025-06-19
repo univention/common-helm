c3fe93e9996b8bb47aa7032f0a1f2903006893c5
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 09:20:12 +0200

    refactor(helm-test-harness): Split provisioning api related tests
    This allows to test username and password separately.

--- helm-test-harness/univention/testing/helm/client/provisioning_api.py
+++ helm-test-harness/univention/testing/helm/client/provisioning_api.py
@@ -5,24 +5,37 @@ from contextlib import nullcontext as does_not_raise
 import subprocess
 
 import pytest
+from pytest_helm.models import HelmTemplateResult
 
 from .base import BaseTest
 
 
-class Auth(BaseTest):
+class AuthPassword(BaseTest):
     """
-    Provisioning API Client configuration checks.
+    Partial client test focused only on the password.
+
+    Checks the following values:
+
+    - `provisioningApi.auth.password`
+    - `provisioningApi.auth.existingSecret`
     """
 
-    config_map_name = None
     secret_name = "release-name-test-nubus-common-provisioning-api"
 
-    default_username = "stub-values-username"
+    secret_default_key = "password"
 
-    path_main_container = "spec.template.spec.containers[?@.name=='main']"
-    path_provisioning_api_username = "data.PROVISIONING_API_USERNAME"
+    path_password = "stringData.password"
 
-    env_password = "PROVISIONING_API_PASSWORD"
+    def get_password(self, result: HelmTemplateResult):
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        return secret.findone(self.path_password)
+
+    def assert_password_value(self, result: HelmTemplateResult, value: str):
+        password = self.get_password(result)
+        assert password == value
+
+    def assert_correct_secret_usage(self, result, *, name=None, key=None):
+        raise NotImplementedError("Use one of the mixins or implement this method.")
 
     def test_auth_plain_values_generate_secret(self, chart):
         values = self.load_and_map(
@@ -33,9 +46,157 @@ class Auth(BaseTest):
                 password: "stub-password"
             """)
         result = chart.helm_template(values)
-        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        self.assert_password_value(result, "stub-password")
 
-        assert secret.findone("stringData.password") == "stub-password"
+    def test_auth_plain_values_password_is_not_templated(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                username: "stub-username"
+                password: "{{ value }}"
+            """)
+        result = chart.helm_template(values)
+        self.assert_password_value(result, "{{ value }}")
+
+    def test_auth_plain_values_password_is_required(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                username: "stub-username"
+                password: null
+            """)
+        with pytest.raises(subprocess.CalledProcessError) as error:
+            chart.helm_template(values)
+        assert "password has to be supplied" in error.value.stderr
+
+    def test_auth_existing_secret_does_not_generate_a_secret(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        with pytest.raises(LookupError):
+            result.get_resource(kind="Secret", name=self.secret_name)
+
+    def test_auth_existing_secret_does_not_require_plain_password(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                password: null
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        with does_not_raise():
+            chart.helm_template(values)
+
+    def test_auth_existing_secret_uses_password(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        self.assert_correct_secret_usage(result, name="stub-secret-name")
+
+    def test_auth_existing_secret_uses_correct_default_key(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+            """)
+        result = chart.helm_template(values)
+        self.assert_correct_secret_usage(result, key="password")
+
+    def test_auth_existing_secret_uses_correct_custom_key(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                existingSecret:
+                  name: "stub-secret-name"
+                  keyMapping:
+                    password: "stub_password_key"
+            """)
+        result = chart.helm_template(values)
+        self.assert_correct_secret_usage(result, key="stub_password_key")
+
+    def test_auth_existing_secret_has_precedence(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                password: "stub-password"
+                existingSecret:
+                  name: "stub-secret-name"
+                  keyMapping:
+                    password: "stub_password_key"
+            """)
+        result = chart.helm_template(values)
+        with pytest.raises(LookupError):
+            result.get_resource(kind="Secret", name=self.secret_name)
+
+        self.assert_correct_secret_usage(result, name="stub-secret-name", key="stub_password_key")
+
+    def test_auth_disabling_existing_secret_by_setting_it_to_null(self, chart):
+        values = self.load_and_map(
+            """
+            provisioningApi:
+              auth:
+                username: "stub-username"
+                password: "stub-password"
+                existingSecret: null
+            """)
+        result = chart.helm_template(values)
+        self.assert_correct_secret_usage(result, name=self.secret_name, key="password")
+
+    def test_global_secrets_keep_is_ignored(self, chart):
+        """
+        Keeping Secrets shall not be supported in Client role.
+
+        Random values for a password will never be generated when in Client
+        role. This is why the configuration `global.secrets.keep` shall not
+        have any effect on Secrets in Client role.
+        """
+        values = self.load_and_map(
+            """
+            global:
+              secrets:
+                keep: true
+
+            provisioningApi:
+              auth:
+                password: "stub-password"
+            """)
+        result = chart.helm_template(values)
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        annotations = secret.findone("metadata.annotations", default={})
+        helm_resource_policy = annotations.get("helm.sh/resource-policy")
+        assert helm_resource_policy != "keep"
+
+class AuthUsername(BaseTest):
+    """
+    Partial client test focused on the username configuration.
+
+    Checks the following values:
+
+    - `provisioningApi.auth.username`
+    """
+
+    config_map_name = None
+
+    default_username = "stub-values-username"
+
+    path_provisioning_api_username = "data.PROVISIONING_API_USERNAME"
 
     def test_auth_plain_values_provide_username_via_config_map(self, chart):
         values = self.load_and_map(
@@ -63,30 +224,6 @@ class Auth(BaseTest):
         config_map = result.get_resource(kind="ConfigMap", name=self.config_map_name)
         assert config_map.findone(self.path_provisioning_api_username) == "stub-value"
 
-    def test_auth_plain_values_password_is_not_templated(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                username: "stub-username"
-                password: "{{ value }}"
-            """)
-        result = chart.helm_template(values)
-        secret = result.get_resource(kind="Secret", name=self.secret_name)
-        assert secret.findone("stringData.password") == "{{ value }}"
-
-    def test_auth_plain_values_password_is_required(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                username: "stub-username"
-                password: null
-            """)
-        with pytest.raises(subprocess.CalledProcessError) as error:
-            chart.helm_template(values)
-        assert "password has to be supplied" in error.value.stderr
-
     def test_auth_username_is_required(self, chart):
         values = self.load_and_map(
             """
@@ -110,138 +247,28 @@ class Auth(BaseTest):
         config_map = result.get_resource(kind="ConfigMap", name=self.config_map_name)
         assert config_map.findone(self.path_provisioning_api_username) == self.default_username
 
-    def test_auth_existing_secret_does_not_generate_a_secret(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                existingSecret:
-                  name: "stub-secret-name"
-            """)
-        result = chart.helm_template(values)
-        with pytest.raises(LookupError):
-            result.get_resource(kind="Secret", name=self.secret_name)
 
-    def test_auth_existing_secret_does_not_require_plain_password(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                password: null
-                existingSecret:
-                  name: "stub-secret-name"
-            """)
-        with does_not_raise():
-            chart.helm_template(values)
+class Auth(AuthPassword, AuthUsername):
+    pass
 
-    def test_auth_existing_secret_used_to_populate_environment_variables(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                existingSecret:
-                  name: "stub-secret-name"
-            """)
-        result = chart.helm_template(values)
+
+class SecretViaEnv:
+    """
+    Mixin which implements the expected Secret usage via environment variables.
+    """
+
+    sub_path_env_password = "env[?@name=='PROVISIONING_API_PASSWORD']"
+
+    def assert_correct_secret_usage(self, result, *, name=None, key=None):
         workload = result.get_resource(kind=self.workload_kind, name=self.workload_name)
-        main_container = workload.findone(self.path_main_container)
+        container = workload.findone(self.path_container)
+        password = container.findone(self.sub_path_env_password)
 
-        password = main_container.findone(f"env[?@name=='{self.env_password}']")
-        assert password.findone("valueFrom.secretKeyRef.name") == "stub-secret-name"
+        if name:
+            assert password.findone("valueFrom.secretKeyRef.name") == name
 
-    def test_auth_existing_secret_uses_correct_default_key(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                existingSecret:
-                  name: "stub-secret-name"
-            """)
-        result = chart.helm_template(values)
-        workload = result.get_resource(kind=self.workload_kind, name=self.workload_name)
-        main_container = workload.findone(self.path_main_container)
-
-        password = main_container.findone(f"env[?@name=='{self.env_password}']")
-        assert password.findone("valueFrom.secretKeyRef.key") == "password"
-
-    def test_auth_existing_secret_uses_correct_custom_key(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                existingSecret:
-                  name: "stub-secret-name"
-                  keyMapping:
-                    password: "stub_password_key"
-            """)
-        result = chart.helm_template(values)
-        workload = result.get_resource(kind=self.workload_kind, name=self.workload_name)
-        main_container = workload.findone(self.path_main_container)
-
-        password = main_container.findone(f"env[?@name=='{self.env_password}']")
-        assert password.findone("valueFrom.secretKeyRef.key") == "stub_password_key"
-
-    def test_auth_existing_secret_has_precedence(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                password: "stub-password"
-                existingSecret:
-                  name: "stub-secret-name"
-                  keyMapping:
-                    password: "stub_password_key"
-            """)
-        result = chart.helm_template(values)
-        with pytest.raises(LookupError):
-            result.get_resource(kind="Secret", name=self.secret_name)
-
-        workload = result.get_resource(kind=self.workload_kind, name=self.workload_name)
-        main_container = workload.findone(self.path_main_container)
-
-        password = main_container.findone(f"env[?@name=='{self.env_password}']")
-        assert password.findone("valueFrom.secretKeyRef.key") == "stub_password_key"
-
-    def test_auth_disabling_existing_secret_by_setting_it_to_null(self, chart):
-        values = self.load_and_map(
-            """
-            provisioningApi:
-              auth:
-                username: "stub-username"
-                password: "stub-password"
-                existingSecret: null
-            """)
-        result = chart.helm_template(values)
-        workload = result.get_resource(kind=self.workload_kind, name=self.workload_name)
-        main_container = workload.findone(self.path_main_container)
-
-        password = main_container.findone(f"env[?@name=='{self.env_password}']")
-        assert password.findone("valueFrom.secretKeyRef.name") == self.secret_name
-        assert password.findone("valueFrom.secretKeyRef.key") == "password"
-
-    def test_global_secrets_keep_is_ignored(self, chart):
-        """
-        Keeping Secrets shall not be supported in Client role.
-
-        Random values for a password will never be generated when in Client
-        role. This is why the configuration `global.secrets.keep` shall not
-        have any effect on Secrets in Client role.
-        """
-        values = self.load_and_map(
-            """
-            global:
-              secrets:
-                keep: true
-
-            provisioningApi:
-              auth:
-                password: "stub-password"
-            """)
-        result = chart.helm_template(values)
-        secret = result.get_resource(kind="Secret", name=self.secret_name)
-        annotations = secret.findone("metadata.annotations", default={})
-        helm_resource_policy = annotations.get("helm.sh/resource-policy")
-        assert helm_resource_policy != "keep"
+        if key:
+            assert password.findone("valueFrom.secretKeyRef.key") == key
 
 
 class Connection(BaseTest):
